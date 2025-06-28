cbdcba947c719d743727fbbdc61d971f225c456a
Andreas Zorn <andreas.zorn@st.oth-regensburg.de>
2025-06-28 12:33:03 +0200

    split to center funktion and one for the angle

--- steering.py
+++ steering.py
@@ -7,13 +7,12 @@ if not pi.connected:
 
 def set_steering_angle(angle):
     angle = max(MAX_LEFT, min(MAX_RIGHT, angle))
-    
-    if angle == 0:
-        pi.set_servo_pulsewidth(SERVO_STEERING_PIN, SERVO_MID)
-    else:
-        target_deg = SERVO_MID_DEG + angle
-        pulsewidth = 1000 + (target_deg / 180.0) * 1000
-        pi.set_servo_pulsewidth(SERVO_STEERING_PIN, pulsewidth)
+    target_deg = SERVO_MID_DEG + angle
+    pulsewidth = 1000 + (target_deg / 180.0) * 1000
+    pi.set_servo_pulsewidth(SERVO_STEERING_PIN, pulsewidth)
+
+def center_steering():
+    pi.set_servo_pulsewidth(SERVO_STEERING_PIN, SERVO_MID_PWM)
 
 def cleanup():
     pi.set_servo_pulsewidth(SERVO_STEERING_PIN, 0)
